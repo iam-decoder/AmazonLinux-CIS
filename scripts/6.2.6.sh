3599bf6ec520a99eb981d6ed794d98d6a012f838
Travis Neal <travis.neal@nielsen.com>
2021-09-27 18:04:06 -0700

    fix syntax

--- scripts/6.2.6.sh
+++ scripts/6.2.6.sh
@@ -2,7 +2,7 @@
 if [ "`echo $PATH | grep ::`" != "" ]; then
     echo "Empty Directory in PATH (::)"
 fi
-if[ "$(echo $PATH | grep :$)" != "" ]; then
+if [ "$(echo $PATH | grep :$)" != "" ]; then
   echo "Trailing : in PATH"
 fi
 p= `echo $PATH | sed -e 's/::/:/' -e 's/:$//' -e 's/:/ /g'` set -- $p
@@ -15,16 +15,18 @@ while [ "$1" != "" ]; do
   if [ -d $1 ]; then
     dirperm= `ls -ldH $1 | cut -f1 -d" "`
     if [ `echo $dirperm | cut -c6`  != "-" ]; then
-echo "Group Write permission set on directory $1" fi
-if [ `echo $dirperm | cut -c9` != "-" ]; then
-echo "Other Write permission set on directory $1"
+      echo "Group Write permission set on directory $1"
+    fi
+    if [ `echo $dirperm | cut -c9` != "-" ]; then
+        echo "Other Write permission set on directory $1"
     fi
     dirown= `ls -ldH $1 | awk '{print $3}'`
     if [ "$dirown" != "root" ] ; then
       echo $1 is not owned by root
     fi
-else
- 318 | P a g e
-echo $1 is not a directory
+  else
+    318 | P a g e
+    echo $1 is not a directory
   fi
-shift done
+shift
+done
